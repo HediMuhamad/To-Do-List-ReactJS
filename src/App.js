1fe55fd9223d0790071bab65843710a60eff1ace
HediMuhamad <hedymuhamad461@gmail.com>
2022-10-14 20:32:49 +0300

    Most of event-handlings added.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,57 @@ import "./App.css";
 import Task from "./components/task/task.component";
 import Button from "./components/button/button.component";
 import TaskInput from "./components/task-input/task-input.component";
+import { useState } from "react";
 
 function App() {
-  const tasks = [
-    "Study to ISA Quiz",
-    "Preparing ISA Report",
-    "Reviewing AI's papers",
-    "DBMS Report about final project with normalization",
-    "MDB Report",
-  ];
+  const [tasks, setTasks] = useState([]);
+  const [newTaskValue, setNewTaskValue] = useState("");
+
+  const addingNewTaskHandler = () => {
+    if (!!newTaskValue && !tasks.includes(newTaskValue)) {
+      setTasks([...tasks, newTaskValue]);
+      setNewTaskValue("");
+    }
+  };
+
+  const taskInputChangedHandler = (event) => {
+    setNewTaskValue(event.target.value);
+  };
+
+  const deleteTaskHandler = (task) => {
+    const newTasks = [];
+
+    tasks.forEach((taskText) => {
+      if (task !== taskText) {
+        newTasks.push(taskText);
+      }
+    });
+
+    setTasks(newTasks);
+  };
 
   return (
     <div className="App">
       <div className="creation-seaction">
-        <TaskInput placeholder={"Add Task"} type={"text"} />
-        <Button label={"Add"} />
+        <TaskInput
+          placeholder={"Add Task"}
+          type={"text"}
+          value={newTaskValue}
+          onChange={taskInputChangedHandler}
+        />
+        <Button label={"Add"} onClick={addingNewTaskHandler} />
       </div>
       <div className="todo-list-section">
         {tasks.map((task) => {
-          return <Task text={task} key={task} />;
+          return (
+            <Task
+              text={task}
+              key={task}
+              onDeleteClick={() => {
+                deleteTaskHandler(task);
+              }}
+            />
+          );
         })}
       </div>
     </div>
